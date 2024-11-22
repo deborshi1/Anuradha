f9f5beba7fd567b9ed9c7316c9c96f492bb89931
deborshi1 <deborshidas349@gmail.com>
2024-11-22 23:07:15 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,20 @@
+
+for (let i =0; i < 15; i++ ){
+    console.log(i);
+    if (i >= 10){
+        continue;
+    }
+}
+ console.log("life after break up");
+ let n =55;
+ while(n > 40){
+    console.log(n);
+    n-=1;
+ }
+ for(let i = 1; i < 10; i++){
+    if (i % 2 ===0){
+        continue;
+    }
+
+    console.log(i);
+ }
